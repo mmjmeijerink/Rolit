d43e9efb1f04dfd1bd41891f3dd05dfac7d274ea
Thijs Scheepers <thijs@infinitedroplets.com>
2011-03-14 13:39:33 +0100

    Games has a color

--- src/rolit/sharedModels/Gamer.java
+++ src/rolit/sharedModels/Gamer.java
@@ -4,10 +4,12 @@ public class Gamer {
 	public String name;
 	public boolean takesPart;
 	private int requestedGameSize;
+	private int color; // Volgens de kleuren gedefineerd in Slot
 	
 	public Gamer() {
 		name = "[NOT CONNECTED]"; // Mag want op een andere manier kan name nooit geset worden met een spatie, dus deze is uniek
 		takesPart = false;
+		color = Slot.EMPTY;
 	}
 	
 	public boolean setRequestedGameSize(int size) {
@@ -23,5 +25,25 @@ public class Gamer {
 	
 	public int getRequestedGameSize() {
 		return requestedGameSize;
+	}
+	
+	public boolean setColor(int aColor) {
+		boolean result = false;
+		if(aColor > -1 && aColor < 5) { 
+			color = aColor;
+			result = true;
+			if(aColor != 0) { 
+				takesPart = true;
+			} else {
+				takesPart = false;
+			}
+		} else {
+			System.out.println("Error: could not change gamer color value because value is invalid.");
+		}
+		return result;
+	}
+	
+	public int getColor() {
+		return color;
 	}
 }
