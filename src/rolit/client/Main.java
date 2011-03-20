2fc0bf259cbb2f8dd9e41e4a083cfa5c33e48ef7
Thijs Scheepers <thijs@infinitedroplets.com>
2011-03-20 17:09:11 +0100

    Niewe versie nummers

--- src/rolit/client/Main.java
+++ src/rolit/client/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationController appController = new ApplicationController();
 		appController.log(	"        Rolit Client by Thijs Scheepers and Mart Meijerink" +
-									"\n         v0.1alpha" +
+									"\n         v1.0alpha" +
 	        						"\n         Protocol of INF2" +
 	        						"\n ----------------------------------------------------------");
 	}
--- src/rolit/server/main.java
+++ src/rolit/server/main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationController appController = new ApplicationController();
 		appController.view().log(	"        Rolit Server by Mart Meijerink and Thijs Scheepers" +
-									"\n         v0.1alpha" +
+									"\n         v1.0beta" +
 	        						"\n         Protocol of INF2" +
 	        						"\n ----------------------------------------------------------");
 	}
