b080e25b55af6360930cf0ce31fe31e664d105ec
Thijs Scheepers <thijs@infinitedroplets.com>
2011-03-21 11:23:58 +0100

    AIController class aangemaakt

---
+++ src/rolit/client/controllers/AIController.java
@@ -0,0 +1,12 @@
+package rolit.client.controllers;
+
+import rolit.sharedModels.Board;
+
+public class AIController {
+	private Board board;
+	
+	public AIController(Board aBoard) {
+		board = aBoard;
+		
+	}
+}
---
+++ src/rolit/test/client/GameViewTest.java
@@ -0,0 +1,17 @@
+package rolit.test.client;
+
+import rolit.client.views.GameView;
+import rolit.sharedModels.Board;
+import rolit.test.board.BoardTest;
+
+public class GameViewTest {
+	private GameView view;
+	
+	public GameViewTest() {
+		view = new GameView();
+	}
+
+	public static void main(String[] args) {
+		GameViewTest gameView = new GameViewTest();
+	}
+}
