3493a4a7c63b91fd831d5bcc857117d06d4db8f9
Thijs Scheepers <thijs@infinitedroplets.com>
2011-03-29 20:20:36 +0200

    JavaDoc (opnieuw)

--- src/rolit/server/models/LoggingInterface.java
+++ src/rolit/server/models/LoggingInterface.java
@@ -1,5 +1,14 @@
 package rolit.server.models;
-
+/**
+ * De logging interface geeft aan of een object kan loggen of niet, als een object kan loggen kan dit bijvoorbeeld naar de UI of naar de Terminal.
+ * Meestal is de ApplicationController een LoggingInterface
+ * @author  Mart Meijerink en Thijs Scheepers
+ * @version 1
+ */
 public interface LoggingInterface {
+	/**
+	 * Als je iets te loggen hebt kan je dat via deze methode doen.
+	 * @param logEntry de te loggen string
+	 */
 	public void log(String logEntry);
 }
